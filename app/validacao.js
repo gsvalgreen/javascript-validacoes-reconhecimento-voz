d935f5ff5c58973ea7a722ab748e14184954774e
Gustavo Valverde <gustavo.valverde5@gmail.com>
2023-09-24 21:37:37 -0300

    Cria jogo numero Secreto

---
+++ app/validacao.js
@@ -0,0 +1,43 @@
+function verificaValidadeChute(chute) {
+  // "+" Automaticamente converte para inteiro
+  // Retorna NaN caso não seja convertido
+  const numero = +chute;
+  if (chuteNaoForNumerico(numero)) {
+    elementoChute.innerHTML += "<div>Valor Inválido</div>";
+    return;
+  }
+  if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+    elementoChute.innerHTML += `
+    <div>Valor Inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`;
+    return;
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+    <h2>Você acertou!</h2>
+    <h3>O número secreto era ${numeroSecreto}</h3>
+    <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+    `;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `<div>
+    O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i>
+    </div>`;
+  } else {
+    elementoChute.innerHTML += `<div>
+    O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i>
+    </div>`;
+  }
+}
+function chuteNaoForNumerico(numero) {
+  return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+  return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "jogar-novamente") {
+    window.location.reload();
+  }
+});
